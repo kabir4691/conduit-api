f88599cb21891fc0f328fd6c65492320f1a87450
Kabir <kabir4691@gmail.com>
2020-05-12 07:47:38 +0530

    Add mongoose model class for user collection

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validations = require('../utils/validations');
+
+const saltRounds = 10;
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: validations.emailRegex
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String,
+    default: ''
+  },
+  image: {
+    type: String,
+    default: ''
+  }
+});
+
+userSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, saltRounds)
+    .then(hashed => {
+      this.password = hashed;
+      next();
+    })
+    .catch(err => next(err)); 
+});
+
+userSchema.methods.verifyPassword = function(password) {
+  console.log('coca cola tu');
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('user', userSchema);
