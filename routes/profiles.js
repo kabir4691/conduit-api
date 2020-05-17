3d53ea37de1970a18b57bf41efe29ba790e45b77
Kabir <kabir4691@gmail.com>
2020-05-17 07:03:56 +0530

    Change authorization check in Get User API to optional

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const profileController = require('../controllers/profileController');
 const authController = require('../controllers/authController');
 
-router.get('/:username', authController.checkAuthorization, profileController.getUser);
+router.get('/:username', authController.checkAuthorizationOptional, profileController.getUser);
 router.post('/:username/follow', authController.checkAuthorization, profileController.followUser);
 router.delete('/:username/follow', authController.checkAuthorization, profileController.unfollowUser)
 
